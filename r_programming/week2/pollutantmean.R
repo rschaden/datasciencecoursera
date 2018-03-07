052cc26fc8c4b85d1cfa73aca3a97a68d4d1b51d
rschaden <rschaden@gmail.com>
2018-03-08 00:23:47 +0100

    Simplify construction of file path

--- r_programming/week2/pollutantmean.R
+++ r_programming/week2/pollutantmean.R
@@ -1,7 +1,8 @@
 pollutantmean <- function(directory, pollutant, id=1:332) {
-    for(i in seq_along(id)) {
-        file_name <- paste(formatC(id[i], width = 3, format = "d", flag = "0"), ".csv", sep = "")
-        path <- file.path(directory, file_name)
+    files = list.files(directory)[id]
+    
+    for(i in seq_along(files)) {
+        path = file.path(directory, files[i])
         
         if(i == 1) {
             data <- read.csv(path)
