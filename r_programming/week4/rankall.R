d062a78d6308213c7dbaebb149666546def253c8
rschaden <rschaden@gmail.com>
2018-03-20 23:08:50 +0100

    Refactor rankall to use lapply with split

--- r_programming/week4/rankall.R
+++ r_programming/week4/rankall.R
@@ -1,10 +1,17 @@
+rankbystate <- function(data, outcome.column, num) {
+    ordered.by.outcome <- data[order(data[, outcome.column], data[, 2]), ]
+    
+    if(num == "best") {
+        num <- 1
+    } else if(num == "worst") {
+        num <- sum(!is.na(ordered.by.outcome[, outcome.column]))
+    }
+    
+    list(hospital=ordered.by.outcome$Hospital.Name[num], state=ordered.by.outcome$State[1])
+}
+
 rankall <- function(outcome, num = "best") {
     data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-    states = unique(data$State)
-    states <- states[order(states)]
-    length <- length(states)
-    
-    result <- data.frame("hospital" = character(length), "state" = character(length), stringsAsFactors=FALSE)
 
     valid.outcomes = c("heart attack", "heart failure", "pneumonia")
     outcome.columns = c(11, 17, 23)
@@ -15,21 +22,6 @@ rankall <- function(outcome, num = "best") {
     outcome.column <- outcome.columns[match(outcome, valid.outcomes)]
     data[, outcome.column] <- as.numeric(data[, outcome.column])
     
-
-    
-    for(i in seq_along(states)) {
-        state <- states[i]
-        filtered.by.state = subset(data, data$State == state)
-        ordered.by.outcome <- filtered.by.state[order(filtered.by.state[, outcome.column], filtered.by.state[, 2]), ]
-        
-        best.hospital <- if(num == "best") {
-            num <- 1
-        } else if(num == "worst") {
-            num <- sum(!is.na(ordered.by.outcome[, outcome.column]))
-        }
-        
-        result[i, ] <- c(ordered.by.outcome$Hospital.Name[num], state)
-    }
-    
-    result
+    result <- lapply(split(data, data$State), rankbystate, outcome.column, num)
+    do.call(rbind, lapply(result, data.frame, stringsAsFactors=FALSE))
 }
